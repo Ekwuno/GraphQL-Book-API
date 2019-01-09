66bfafbb582afde0dd10a8742895e5adee474ce3
Ekwuno Obinna <obinnacodes@gmail.com>
2019-01-09 21:50:58 +0100

    setting up express and express-graphql

--- server/app.js
+++ server/app.js
@@ -8,5 +8,5 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 app.listen(4000, ()=>{
-    console.log('now listening for requests on port 4000' );
+    console.log('now listening for requests on port 4000');
 })
