48b2997c6be50bc69fa562e2e03ef0e3253cd527
Ekwuno Obinna <obinnacodes@gmail.com>
2019-01-18 20:54:52 +0100

    setting up react boiler plate for client

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Works
+        </h1>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
